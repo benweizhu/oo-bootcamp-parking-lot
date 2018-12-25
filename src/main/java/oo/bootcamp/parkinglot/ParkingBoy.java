8272d48b141b26e6ef0f5a8faaafc9cb5da49684
Benwei Zhu <benweizhu@gmail.com>
2018-12-25 22:47:08 +0800

    task 4

--- src/main/java/oo/bootcamp/parkinglot/ParkingBoy.java
+++ src/main/java/oo/bootcamp/parkinglot/ParkingBoy.java
@@ -1,18 +1,48 @@
 package oo.bootcamp.parkinglot;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
 public class ParkingBoy {
 
-  private ParkingLot parkingLot;
+  private List<ParkingLot> parkingLots;
 
-  public ParkingBoy(ParkingLot parkingLot) {
-    this.parkingLot = parkingLot;
+  public ParkingBoy(List<ParkingLot> parkingLots) {
+    this.parkingLots = parkingLots;
   }
 
-  public Ticket park(Car car) {
-    return this.parkingLot.parking(car);
+  public Ticket park(Car car) throws Throwable {
+    return this.parkingLots
+        .stream()
+        .map(parkingLot -> getTicket(car, parkingLot))
+        .filter(Objects::nonNull)
+        .findAny()
+        .orElseThrow((Supplier<Throwable>) NoSlotException::new);
   }
 
-  public Car pick(Ticket ticket) {
-    return this.parkingLot.pick(ticket);
+  private Ticket getTicket(Car car, ParkingLot parkingLot) {
+    try {
+      return parkingLot.parking(car);
+    } catch (NoSlotException e) {
+      return null;
+    }
+  }
+
+  public Car pick(Ticket ticket) throws Throwable {
+    return this.parkingLots
+        .stream()
+        .map(parkingLot -> getCar(ticket, parkingLot))
+        .filter(Objects::nonNull)
+        .findAny()
+        .orElseThrow((Supplier<Throwable>) PickCarFailedException::new);
+  }
+
+  private Car getCar(Ticket ticket, ParkingLot parkingLot) {
+    try {
+      return parkingLot.pick(ticket);
+    } catch (PickCarFailedException e) {
+      return null;
+    }
   }
 }
--- src/test/java/oo/bootcamp/parkinglot/ParkingBoyTest.java
+++ src/test/java/oo/bootcamp/parkinglot/ParkingBoyTest.java
@@ -2,22 +2,31 @@ package oo.bootcamp.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ParkingBoyTest {
 
   @Test
-  void should_return_ticket_when_parking_boy_park_car_given_parking_lot_A_is_available() {
-    ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(1));
+  void should_return_ticket_when_parking_boy_park_car_given_parking_lot_A_is_available() throws Throwable {
+    ParkingLot parkingLot = new ParkingLot(1);
+    ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+    parkingLots.add(parkingLot);
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
     Ticket ticket = parkingBoy.park(new Car());
 
     assertNotNull(ticket);
   }
 
   @Test
-  void should_return_same_car_when_parking_boy_pick_car_given_correct_ticket() {
-    ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(1));
+  void should_return_same_car_when_parking_boy_pick_car_given_correct_ticket() throws Throwable {
+    ParkingLot parkingLot = new ParkingLot(1);
+    ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+    parkingLots.add(parkingLot);
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
     Car car = new Car();
     Ticket ticket = parkingBoy.park(car);
     Car pickedCar = parkingBoy.pick(ticket);
@@ -26,13 +35,29 @@ public class ParkingBoyTest {
   }
 
   @Test
-  void should_return_same_car_when_customer_pick_car_given_correct_ticket() {
+  void should_return_same_car_when_customer_pick_car_given_correct_ticket() throws Throwable {
     ParkingLot parkingLot = new ParkingLot(1);
-    ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+    ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+    parkingLots.add(parkingLot);
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
     Car car = new Car();
     Ticket ticket = parkingBoy.park(car);
     Car pickedCar = parkingLot.pick(ticket);
 
     assertSame(car, pickedCar);
+  }
+
+  @Test
+  void should_return_ticket_when_parking_boy_park_car_given_parking_lot_A_is_full_parking_lot_b_is_available() throws Throwable {
+    List<ParkingLot> parkingLots = new ArrayList<>();
+    ParkingLot parkingLotA = new ParkingLot(1);
+    parkingLotA.parking(new Car());
+    parkingLots.add(parkingLotA);
+    ParkingLot parkingLotB = new ParkingLot(1);
+    parkingLots.add(parkingLotB);
+
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+
+    assertNotNull(parkingBoy.park(new Car()));
   }
 }
