9add3231ccf88e0a0bc9b897239ff625ef9a9708
Benwei Zhu <benweizhu@gmail.com>
2018-12-25 22:11:24 +0800

    task 2

--- src/main/java/oo/bootcamp/parkinglot/ParkingBoy.java
+++ src/main/java/oo/bootcamp/parkinglot/ParkingBoy.java
@@ -1,7 +1,18 @@
 package oo.bootcamp.parkinglot;
 
 public class ParkingBoy {
+
+  private ParkingLot parkingLot;
+
+  public ParkingBoy(ParkingLot parkingLot) {
+    this.parkingLot = parkingLot;
+  }
+
   public Ticket park(Car car) {
-    return new Ticket();
+    return this.parkingLot.parking(car);
+  }
+
+  public Car pick(Ticket ticket) {
+    return this.parkingLot.pick(ticket);
   }
 }
--- src/test/java/oo/bootcamp/parkinglot/ParkingBoyTest.java
+++ src/test/java/oo/bootcamp/parkinglot/ParkingBoyTest.java
@@ -3,12 +3,24 @@ package oo.bootcamp.parkinglot;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ParkingBoyTest {
+
   @Test
   void should_return_ticket_when_parking_boy_park_car_given_parking_lot_A_is_available() {
-    ParkingBoy parkingBoy = new ParkingBoy();
+    ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(1));
     Ticket ticket = parkingBoy.park(new Car());
     assertNotNull(ticket);
+  }
+
+  @Test
+  void should_return_same_car_when_parking_boy_pick_car_given_correct_ticket() {
+    ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(1));
+    Car car = new Car();
+    Ticket ticket = parkingBoy.park(car);
+    Car pickedCar = parkingBoy.pick(ticket);
+
+    assertSame(car, pickedCar);
   }
 }
