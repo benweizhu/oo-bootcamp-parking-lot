a136883a375dc7bdf95f331fec9fd83220a9cb4f
Benwei Zhu <benweizhu@gmail.com>
2018-12-25 22:13:42 +0800

    task 3

--- src/test/java/oo/bootcamp/parkinglot/ParkingBoyTest.java
+++ src/test/java/oo/bootcamp/parkinglot/ParkingBoyTest.java
@@ -11,6 +11,7 @@ public class ParkingBoyTest {
   void should_return_ticket_when_parking_boy_park_car_given_parking_lot_A_is_available() {
     ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot(1));
     Ticket ticket = parkingBoy.park(new Car());
+
     assertNotNull(ticket);
   }
 
@@ -20,6 +21,17 @@ public class ParkingBoyTest {
     Car car = new Car();
     Ticket ticket = parkingBoy.park(car);
     Car pickedCar = parkingBoy.pick(ticket);
+
+    assertSame(car, pickedCar);
+  }
+
+  @Test
+  void should_return_same_car_when_customer_pick_car_given_correct_ticket() {
+    ParkingLot parkingLot = new ParkingLot(1);
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+    Car car = new Car();
+    Ticket ticket = parkingBoy.park(car);
+    Car pickedCar = parkingLot.pick(ticket);
 
     assertSame(car, pickedCar);
   }
