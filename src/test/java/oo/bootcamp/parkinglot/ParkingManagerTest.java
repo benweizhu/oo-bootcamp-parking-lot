9c7f8fa1deea5bcd7a405771305033ef0f70e12f
Benwei Zhu <benweizhu@gmail.com>
2019-01-03 15:09:26 +0800

    task 3

--- src/test/java/oo/bootcamp/parkinglot/ParkingManagerTest.java
+++ src/test/java/oo/bootcamp/parkinglot/ParkingManagerTest.java
@@ -39,4 +39,25 @@ public class ParkingManagerTest {
     assertNotNull(ticket);
     assertSame(car, parkingLotA.pick(ticket));
   }
+
+  @Test
+  void should_park_car_to_parking_lot_a_when_parking_manager_park_car_given_parking_manager_has_a_parking_boy_with_parking_lot_a_and_a_smart_parking_boy_with_parking_lot_b() throws Throwable {
+      ArrayList<SuperBoy> parkingBoys = new ArrayList<>();
+
+      ParkingLot parkingLotA = new ParkingLot(10);
+      ArrayList<ParkingLot> parkingLotsA = new ArrayList<>();
+      parkingLotsA.add(parkingLotA);
+      parkingBoys.add(new SmartParkingBoy(parkingLotsA));
+
+      ParkingLot parkingLotB = new ParkingLot(10);
+      ArrayList<ParkingLot> parkingLotsB = new ArrayList<>();
+      parkingLotsB.add(parkingLotB);
+      parkingBoys.add(new ParkingBoy(parkingLotsB));
+
+      ParkingManager parkingManager = new ParkingManager(parkingBoys);
+      Car car = new Car();
+      Ticket ticket = parkingManager.park(car);
+      assertNotNull(ticket);
+      assertSame(car, parkingLotA.pick(ticket));
+  }
 }
