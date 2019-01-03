8e9803f5ec7f9f4d7aeda802d04dfe7f596f0ac5
Benwei Zhu <benweizhu@gmail.com>
2019-01-03 17:10:51 +0800

    task 7

--- src/test/java/oo/bootcamp/parkinglot/ParkingManagerTest.java
+++ src/test/java/oo/bootcamp/parkinglot/ParkingManagerTest.java
@@ -117,4 +117,32 @@ public class ParkingManagerTest {
     Ticket ticket = parkingManager.park(car);
     assertSame(car, parkingLot.pick(ticket));
   }
+
+  @Test
+  void should_park_car_to_parking_lot_a_when_parking_manager_park_car_given_parking_manager_has_a_parking_boy_with_parking_lot_a_fulled_and_a_smart_parking_boy_with_parking_lot_b_fulled_and_manager_has_available_parking_lot() {
+    ParkingLot parkingLotA = new ParkingLot(1);
+    parkingLotA.park(new Car());
+    ArrayList<ParkingLot> parkingLotsA = new ArrayList<>();
+    parkingLotsA.add(parkingLotA);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotsA);
+
+    ParkingLot parkingLotB = new ParkingLot(1);
+    parkingLotB.park(new Car());
+    ArrayList<ParkingLot> parkingLotsB = new ArrayList<>();
+    parkingLotsB.add(parkingLotB);
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLotsB);
+
+    ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+    ParkingLot parkingLotC = new ParkingLot(2);
+    parkingLots.add(parkingLotC);
+
+    ParkingManager parkingManager = new ParkingManager(parkingLots);
+    parkingManager.addParkingBoy(smartParkingBoy);
+    parkingManager.addParkingBoy(parkingBoy);
+
+    Car car = new Car();
+
+    Ticket ticket = parkingManager.park(car);
+    assertSame(car, parkingLotC.pick(ticket));
+  }
 }
